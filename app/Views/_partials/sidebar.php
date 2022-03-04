74fa249676426ec7e153a3d92970154d68a91a05
ardi renaldi <renaldimedia@gmail.com>
2022-03-04 14:40:48 +0700

    add menu events

--- app/Views/_partials/sidebar.php
+++ app/Views/_partials/sidebar.php
@@ -115,8 +115,8 @@
         <div id="event" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <!-- <h6 class="collapse-header">Custom Components:</h6> -->
-                <a class="collapse-item" href="buttons.html">List Event</a>
-                <a class="collapse-item" href="buttons.html">Event baru</a>
+                <a class="collapse-item" href="<?= base_url('events') ?>">List Event</a>
+                <a class="collapse-item" href="<?= base_url('events/add') ?>">Event baru</a>
             </div>
         </div>
     </li>
